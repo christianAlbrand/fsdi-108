872bfc6e5a41d0b687414402a304c36744e19376
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2024-11-14 20:21:46 -0800

    fsdi-108-install-packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
+function App(){
+  return(
+    <div className = 'App'>
+      <Navbar></Navbar>
+      <div className='content'>
+        <h1>Hello World React!</h1>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "./styles/footer.css"
+
+function Footer(){
+    return(
+        <div className="footer_container">
+            <h2 className="owner_name">Created by: Christian Albrand Aguirre</h2>
+        </div>
+    );
+}
+
+export default Footer;
